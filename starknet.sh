60c18b1c9559ba29c5950b27e38bc760c7efb644
opmklinh <61728239+opmklinh@users.noreply.github.com>
2024-04-10 12:10:44 +0700

    Initial commit

---
+++ starknet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-f739c773-630d-4c33-985d-47264be9416c/"
+
+OUTPUT_DIR="logs"
+
+starknet_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"starknet_blockNumber"}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$starknet_data" >> "$OUTPUT_DIR/starknet.txt"
+    
+    echo "StarkNet data fetched and saved to $OUTPUT_DIR/starknet.txt."
+else
+    echo "Failed to fetch StarkNet data."
+fi
